1d33cc0906b640b060241039aa167637c8c637b1
Romain <romain@ex-alto.com>
2018-05-10 15:10:09 +0200

    Configuration de la langue fr

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.21 on 2018-05-10 12:45:07.
+ * Generated for Laravel 5.6.21 on 2018-05-10 13:09:31.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
