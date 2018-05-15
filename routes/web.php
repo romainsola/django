60533a54bc221aed50dcd6044f4d312021400d2b
Romain <romain@ex-alto.com>
2018-05-15 22:33:05 +0200

    Suppression Theme, Bootstrap de base

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/gestion', 'HomeController@index')->name('gestion');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,26 +14,3 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.less('resources/assets/theme/less/theme.less', 'public/css/build_theme.css')
-    .sourceMaps();
-
-mix.styles([
-    'public/css/build_theme.css',
-    'resources/assets/theme/css/icons/icomoon/styles.css'
-], 'public/css/theme.css')
-    .sourceMaps();
-
-mix.babel([
-    'resources/assets/theme/js/plugins/loaders/pace.min.js',
-    'resources/assets/theme/js/plugins/loaders/blockui.min.js',
-    'resources/assets/theme/js/core/libraries/jquery.min.js',
-    'resources/assets/theme/js/core/libraries/bootstrap.min.js',
-    'resources/assets/theme/js/plugins/forms/styling/switchery.min.js',
-    'resources/assets/theme/js/plugins/forms/styling/uniform.min.js',
-    'resources/assets/theme/js/plugins/forms/selects/select2.min.js',
-    'resources/assets/theme/js/plugins/uploaders/fileinput/fileinput.min.js',
-    'resources/assets/theme/js/core/app.js',
-    'resources/assets/theme/js/plugins/forms/inputs/typeahead/typeahead.b',
-    'resources/assets/theme/js/plugins/ui/ripple.min.js',
-    'resources/assets/theme/js/pages/login.js',
-], 'public/js/theme.js');
